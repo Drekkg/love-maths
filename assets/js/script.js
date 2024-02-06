195ea0aa8be9983540b14ead74973fcfa266025e
drekkg <rizzagum@gmail.com>
2024-02-06 22:57:24 +0100

    adds code to generate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
+/**
+ * The main game loop first loaded when the script is called 
+ * and after the users answer has been processed
+ */
 function rungame(){
-
+    //creates two random numbers between 1 and 25 
+    let num1 = Math.floor(Math.random() *25) +1;
+    let num2 = Math.floor(Math.random() *25) +1;
 };
+
 function checkAnswer() {
 
 }
