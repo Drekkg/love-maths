29af1cad3ce1a438f23c025afde3a5ee455617e8
drekkg <rizzagum@gmail.com>
2024-02-08 18:49:02 +0100

    FEAT adds subtract gsmr code

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,6 +67,8 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, "addition"];
   } else if(operator === "x"){
     return [operand1 * operand2, "multiply"];
+  }else if(operator === "-"){
+    return[operand1 - operand2, "subtract"]
   }else {
     alert("umimplemented operator");
   }
@@ -91,8 +93,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-  document.getElementById("operand1").textContent = operand1;
-  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent = operand2 < operand1 ? operand2 : operand1;
   document.getElementById("operator").textContent = "-";
 }
 
